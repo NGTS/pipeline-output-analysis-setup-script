819119fc84a4f88190752697f126d16039a4a22c
Simon Walker <s.r.walker101@googlemail.com>
2015-09-07 11:09:47 +0100

    Import required packages

--- setup_directory.py
+++ setup_directory.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import division, print_function, absolute_import
 import argparse
 import os
 import subprocess as sp
+from contextlib import contextmanager
+import tempfile
+try:
+    import urllib.request as urllib2
+except ImportError:
+    import urllib2
 
 MINICONDA_URL = 'https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh'
 
